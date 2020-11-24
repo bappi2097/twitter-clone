2cd38a152b31379b41ea659f476083916d7cd0b6
bappi2097 <bappi35-2097@diu.edu.bd>
2020-11-25 01:00:59 +0600

    bug fixed

--- resources/views/_tweet.blade.php
+++ resources/views/_tweet.blade.php
@@ -1,7 +1,7 @@
 <div class="flex p-4 {{$loop->last ? '' : 'border-b border-b-gray-300'}}">
     <div class="mr-2 flex-shrink-0">
         <a href="{{$tweet->user->path()}}">
-            <img class="rounded-full mr-2" src="{{auth()->user()->avatar}}" alt="" height="50" width="50">
+            <img class="rounded-full mr-2" src="{{$tweet->user->avatar}}" alt="" height="50" width="50">
         </a>
     </div>
     <div>
